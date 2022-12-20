0e9251521d11c96060e6528abf1388962306090d
cooldudeseven7 <100153927+cooldudeseven7Dev@users.noreply.github.com>
2022-12-20 10:08:11 -0500

    RUN THIS FILE!
    UPDATE

--- OS_R2.py
+++ OS_R2.py
@@ -1,4 +1,5 @@
 #System Codename: Vesti
+App1_Installed = 'TST' #placeholder for future update
 from tkinter import * #type: ignore
 from tkinter import simpledialog,messagebox
 class System:
@@ -11,7 +12,7 @@ class System:
             cs.geometry('750x750')
             lt1 = Label(cs,text='Your System Crashed With Message:\n'+errcode+'\n \nError Code:\n0X000') # Ex. KERNAL~CRASH~ERROR: SYSTEMOSMCFKERNAL
             lt1.pack()
-            q = Button(cs,text='Quit System',command=System.core_main.os_UserItems.cmdApp.quit)
+            q = Button(cs,text='Quit System [Reboot is NOT availble unless you re-open the runner.]',command=System.core_main.os_UserItems.cmdApp.quit)
             q.pack()
             cs.mainloop()
         class bootmgr:
@@ -21,21 +22,21 @@ class System:
                 mgrw.geometry('500x500')
                 ML = Label(mgrw,text='kernal ~ boot manager\nversion: '+str(System.kernal.bootmgr.info.version)) #type: ignore
                 ML.pack()
-                bn = Button(mgrw,text='Boot Normally',command=mgrw.destroy)
+                bn = Button(mgrw,text='Boot Normally - Just DESTROY this window',command=mgrw.destroy)
                 bn.pack()
                 q = Button(mgrw,text='Quit System FULLY',command=System.core_main.os_UserItems.cmdApp.quit)
                 q.pack()
                 mgrw.mainloop()
             class info:
-                version = 0.1
+                version = '0.1.1'
     class Bios_UEFI:
         class framework:
             class info:
-                ver = '0.4.2'
-                build = 'KU-FU-AU-RD~12/18/2022:10:53AM' #AU = APPS UPDATE #F/KU = KERNAL/FRAMEWORK UPDATE #GUIU/UIU = GUI USER INTERFACE UPDATE/USER INTERFACE UPDATE #RD = RELEASE DATE #BF = BUG FIX #M GUI/UI C = MINI GUI/UI CJANGE
-                updateLog = '\nBoot Manager Added\n Crash Added Incase of unknown actions.'
+                ver = '0.4.3_GHREPO_0.2'
+                build = 'AU~12/20/2022:9:58AM' #AU = APPS UPDATE #F/KU = KERNAL/FRAMEWORK UPDATE #GUIU/UIU = GUI USER INTERFACE UPDATE/USER INTERFACE UPDATE #RD = RELEASE DATE #BF = BUG FIX #M GUI/UI C = MINI GUI/UI CJANGE
+                updateLog = 'App Update, Help Menu Update, Repo Updated.\nPrev Update: Boot Manager Added\n Prev Update: Crash Added Incase of unknown actions.'
             class app: # System.Bios_UEFI.framework.app.main_app
-                ver = '0.1.7.5'
+                ver = '0.1.7.7'
                 def main_app(): #type: ignore
                     root_fwai = Tk()
                     root_fwai.geometry('500x500')
@@ -57,7 +58,7 @@ class System:
                 def lhb_app(): #type: ignore
                     lhbw = Tk()
                     lhbw.title('Help - Login')
-                    hll1=Label(lhbw,text='Help - Login\n-----------\n \nI Dont see my username\nThis may be because the update that comes with your profile has not come out, or you have an outdated version of the OS.\nSorry! There seems to be no more help articles at this time. ¯\_(ツ)_/¯') #type: ignore
+                    hll1=Label(lhbw,text='Help - Login\n-----------\n \nI Dont see my username\nThis may be because the update that comes with your profile has not come out, or you have an outdated version of the OS.\nWhy can i see my password in the code?\n We dont have actual servers and people who check this code can find it. Sorry! [Make sure to use a password that is not common for you when you create an account!]\nSorry! There seems to be no more help articles at this time. ¯\_(ツ)_/¯') #type: ignore
                     hll1.pack()
     class core_main:
         def OS_m_PLS(): #type: ignore #PLS = Passed Login Screen # m = main 
@@ -66,18 +67,17 @@ class System:
             root.geometry('800x800')
             qb = Button(root,text='Quit',command=System.core_main.os_UserItems.cmdApp.quit)
             qb.pack()
-            fwaiB = Button(root,text='System Information',command=System.Bios_UEFI.framework.app.main_app)
+            fwaiB = Button(root,text='Vesti OS System Information',command=System.Bios_UEFI.framework.app.main_app)
             fwaiB.pack()
             wm = Button(root,text='Window Maker',command=System.core_main.os_UserItems.apps.WindowMaker.app.main_app.WindowMaker)
             wm.pack()
             vs = Button(root,text='Vesti Store',command=System.core_main.os_UserItems.apps.Vesti.store.app.main)
-            vs.pack()
+            # vs.pack()
             # Write Above
             root.mainloop()
         def prepBoot(): #type: ignore
             lwindow.destroy()
             System.kernal.bootmgr.main()
-            messagebox.showinfo('Message','Its okay to be a guest, But get your own account and password [People can inspect code and see the password. Please, dont use a common password. Dont worry, Everybody has the same apps in their user accounts. Check github repo for more information on this topic.]')
             System.core_main.OS_m_PLS()
         class os_UserItems:
             class apps:
@@ -90,7 +90,7 @@ class System:
                                 wmroot=Tk() #wmroot=window maker root
                                 wmLabel = Label(wmroot,text='Window Maker '+System.core_main.os_UserItems.apps.WindowMaker.app.data.ver+'\n------\n \nPlease enter the prompts displayed to make a window.')  # type: ignore
                                 wmLabel.pack()
-                                type = simpledialog.askstring('Window Maker','Type of window (Warning,Error,Info)')
+                                type = simpledialog.askstring('Window Maker','Type of window (Warning,Error,Info <== SPELLED LIKE THIS)')
                                 title = simpledialog.askstring('Window Maker','Title Bar Text')
                                 text = simpledialog.askstring('Window Maker','Enter the text you want to display on the window.')
                                 if type=='Error':
@@ -127,7 +127,7 @@ class System:
                 def quit(): #type: ignore
                     q = messagebox.askyesno('Quit','Are you sure you you want to quit?')
                     if q==True:
-                        quit('System - Quit - UserActivated - Stop Code: U-A_Q1')
+                        quit('System - Quit - UserActivated - Stop Code: UA_NAC_Q1')
 lwindow = Tk()
 l = Label(lwindow,text='Please Select A User')
 u1b = Button(lwindow,text='Guest/Unregistered',command=System.core_main.prepBoot) #u1b = User 1 Button [Use var template]
@@ -137,5 +137,4 @@ l.pack()
 u1b.pack()
 l2.pack()
 hb.pack()
-lwindow.mainloop() #123 Lines '>'        <====See the face?
-#                              o         <====See the face?
+lwindow.mainloop()
